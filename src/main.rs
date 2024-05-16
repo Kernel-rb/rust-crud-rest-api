e458eed154d2677cb79f93b2932b959fb0a49b60
Kernel-rb <matabsaifeddine@gmail.com>
2024-05-16 16:47:10 +0100

    delete dead code

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,6 @@ const DATABASE_URL: &str =  env!("DATABASE_URL");
 // HTTP RESPONSE
 const OK_RESPONSE: &str = "HTTP/1.1 200 OK\r\n\\Content-Type: application/json\r\n\r\n";
 const NOT_FOUND: &str = "HTTP/1.1 404 NOT FOUND\r\n\\Content-Type: application/json\r\n\r\n";
-const BAD_REQUEST: &str = "HTTP/1.1 400 BAD REQUEST\r\n\\Content-Type: application/json\r\n\r\n";
 const INTERNAL_SERVER_ERROR: &str = "HTTP/1.1 500 INTERNAL SERVER ERROR\r\n\\Content-Type: application/json\r\n\r\n";
 
 // Main function
